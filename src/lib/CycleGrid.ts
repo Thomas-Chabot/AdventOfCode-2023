c1e8c342545bf34c0c3bcda39f789770dc1ca390
Wiggles <seanhodges@cmail.carleton.ca>
2023-12-16 15:22:19 -0500

    Implement Day 14 (#13)
    * Begin Day 14
    
    * Implement Part 1
    
    * Implement Day 14 Part 2
    
    ---------
    
    Co-authored-by: Sean Hodges <sean.hodges@nserc-crsng.gc.ca>

---
+++ src/lib/CycleGrid.ts
@@ -0,0 +1,3 @@
+/*
+    This function takes in a grid of values and cycles the grid, such that values will be anchored to a direction.
+*/
--- src/solutions/index.ts
+++ src/solutions/index.ts
@@ -11,3 +11,4 @@ export * from "./Day10";
 export * from "./Day11";
 export * from "./Day12";
 export * from "./Day13";
+export * from "./Day14";
